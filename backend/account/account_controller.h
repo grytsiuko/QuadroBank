e0fe7c15fba664f0bf2830c0832de29aa1e35f50
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-10-24 23:54:21 +0300

    Exception class and moved exception handler to controller

--- backend/account/account_controller.h
+++ backend/account/account_controller.h
@@ -19,23 +19,48 @@ private:
 public:
 
     Response<SessionTokenDto> authorize(const AccountAuthorizeDto &account_authorize_dto) const {
-        return _account_service.authorize(account_authorize_dto);
+        try{
+            SessionTokenDto *result = _account_service.authorize(account_authorize_dto);
+            return Response<SessionTokenDto>::success(result);
+        } catch (const Exception &exception) {
+            return Response<SessionTokenDto>::error(new string(exception.get_message()));
+        }
     }
 
     Response<AccountBalanceDto> check_balance(const SessionTokenDto &sessionTokenDto) const {
-        return _account_service.check_balance(sessionTokenDto);
+        try {
+            AccountBalanceDto *result = _account_service.check_balance(sessionTokenDto);
+            return Response<AccountBalanceDto>::success(result);
+        } catch (const Exception &exception) {
+            return Response<AccountBalanceDto>::error(new string(exception.get_message()));
+        }
     }
 
     Response<void> top_up(const AccountUpdateDto &account_update_dto) const {
-        return _account_service.top_up(account_update_dto);
+        try {
+            _account_service.top_up(account_update_dto);
+            return Response<void>::success();
+        } catch (const Exception &exception) {
+            return Response<void>::error(new string(exception.get_message()));
+        }
     }
 
     Response<void> withdraw(const AccountUpdateDto &account_update_dto) const {
-        return _account_service.withdraw(account_update_dto);
+        try {
+            _account_service.withdraw(account_update_dto);
+            return Response<void>::success();
+        } catch (const Exception &exception) {
+            return Response<void>::error(new string(exception.get_message()));
+        }
     }
 
     Response<void> transfer(const AccountTransferDto &account_transfer_dto) const {
-        return _account_service.transfer(account_transfer_dto);
+        try {
+            _account_service.transfer(account_transfer_dto);
+            return Response<void>::success();
+        } catch (const Exception &exception) {
+            return Response<void>::error(new string(exception.get_message()));
+        }
     }
 };
 
--- backend/account/account_service.h
+++ backend/account/account_service.h
@@ -11,6 +11,7 @@
 #include "../token/token_service.h"
 #include "dto/account_update_dto.h"
 #include "dto/account_transfer_dto.h"
+#include "../utils/exception.h"
 
 class AccountService : public Singleton<AccountService> {
 
@@ -27,94 +28,91 @@ private:
 
 public:
 
-    Response<SessionTokenDto> authorize(const AccountAuthorizeDto &account_authorize_dto) const {
+    SessionTokenDto *authorize(const AccountAuthorizeDto &account_authorize_dto) const {
         Optional<Account> account = _account_repository.get_by_card_number(account_authorize_dto._card_number);
         if (account.is_empty()) {
-            return Response<SessionTokenDto>::error(new string("No such card number"));
+            throw Exception("No such card number");
         }
 
         if (account.get()->_pin != account_authorize_dto._pin) {
-            return Response<SessionTokenDto>::error(new string("Illegal pin"));
+            throw Exception("Illegal pin");
         }
 
-        auto *dto = new SessionTokenDto{
+        return new SessionTokenDto{
                 _token_service.generate_token(*account.get())
         };
-        return Response<SessionTokenDto>::success(dto);
     }
 
-    Response<AccountBalanceDto> check_balance(const SessionTokenDto &session_token_dto) const {
+    AccountBalanceDto *check_balance(const SessionTokenDto &session_token_dto) const {
         string card_number = _token_service.get_card_number(session_token_dto._token);
         Optional<Account> account = _account_repository.get_by_card_number(card_number);
 
         if (account.is_empty()) {
-            return Response<AccountBalanceDto>::error(new string("Illegal token"));
+            throw Exception("Illegal token");
         }
 
-        return Response<AccountBalanceDto>::success(new AccountBalanceDto(*account.get()));
+        return new AccountBalanceDto(*account.get());
     }
 
-    Response<void> top_up(const AccountUpdateDto &account_update_dto) const {
+    void top_up(const AccountUpdateDto &account_update_dto) const {
         if (account_update_dto._sum <= 0) {
-            return Response<void>::error(new string("Sum should be positive"));
+            throw Exception("Sum should be positive");
         }
 
         string card_number = _token_service.get_card_number(account_update_dto._token);
         Optional<Account> account = _account_repository.get_by_card_number(card_number);
 
         if (account.is_empty()) {
-            return Response<void>::error(new string("Illegal token"));
+            throw Exception("Illegal token");
         }
 
         Account account_copy = Account(*account.get());
         account_copy._balance += account_update_dto._sum;
         _account_repository.update(account_copy);
-        return Response<void>::success();
     }
 
-    Response<void> withdraw(const AccountUpdateDto &account_update_dto) const {
+    void withdraw(const AccountUpdateDto &account_update_dto) const {
         if (account_update_dto._sum <= 0) {
-            return Response<void>::error(new string("Sum should be positive"));
+            throw Exception("Sum should be positive");
         }
 
         string card_number = _token_service.get_card_number(account_update_dto._token);
         Optional<Account> account = _account_repository.get_by_card_number(card_number);
 
         if (account.is_empty()) {
-            return Response<void>::error(new string("Illegal token"));
+            throw Exception("Illegal token");
         }
 
         Account account_copy = Account(*account.get());
         account_copy._balance -= account_update_dto._sum;
 
         if (!account_copy._is_credit && account_copy._balance < 0) {
-            return Response<void>::error(new string("You have simple card, you cannot have negative balance"));
+            throw Exception("You have simple card, you cannot have negative balance");
         }
 
         if (account_copy._is_credit && account_copy._balance < -account_copy._credit_limit) {
-            return Response<void>::error(new string("You cannot exceed your credit limit"));
+            throw Exception("You cannot exceed your credit limit");
         }
 
         _account_repository.update(account_copy);
-        return Response<void>::success();
     }
 
-    Response<void> transfer(const AccountTransferDto &account_transfer_dto) const {
+    void transfer(const AccountTransferDto &account_transfer_dto) const {
         if (account_transfer_dto._sum <= 0) {
-            return Response<void>::error(new string("Sum should be positive"));
+            throw Exception("Sum should be positive");
         }
 
         string card_number = _token_service.get_card_number(account_transfer_dto._token);
         Optional<Account> account = _account_repository.get_by_card_number(card_number);
 
         if (account.is_empty()) {
-            return Response<void>::error(new string("Illegal token"));
+            throw Exception("Illegal token");
         }
 
         Optional<Account> target_account = _account_repository.get_by_card_number(account_transfer_dto._target_card);
 
         if (target_account.is_empty()) {
-            return Response<void>::error(new string("No such target card"));
+            throw Exception("No such target card");
         }
 
         Account account_copy = Account(*account.get());
@@ -123,16 +121,15 @@ public:
         target_account_copy._balance += account_transfer_dto._sum;
 
         if (!account_copy._is_credit && account_copy._balance < 0) {
-            return Response<void>::error(new string("You have simple card, you cannot have negative balance"));
+            throw Exception("You have simple card, you cannot have negative balance");
         }
 
         if (account_copy._is_credit && account_copy._balance < -account_copy._credit_limit) {
-            return Response<void>::error(new string("You cannot exceed your credit limit"));
+            throw Exception("You cannot exceed your credit limit");
         }
 
         _account_repository.update(account_copy);
         _account_repository.update(target_account_copy);
-        return Response<void>::success();
     }
 };
 
