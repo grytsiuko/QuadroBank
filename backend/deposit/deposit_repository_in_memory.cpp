3827c183afaa75773b33845b78ca181cbaa3f5e5
hronograf <vlad.boholii@gmail.com>
2020-10-29 20:18:10 +0200

    deposit repository methods to cpp

---
+++ backend/deposit/deposit_repository_in_memory.cpp
@@ -0,0 +1,39 @@
+#include "deposit_repository_in_memory.h"
+
+int DepositRepositoryInMemory::_freeId = 1;
+
+DepositRepositoryInMemory::DepositRepositoryInMemory() {
+    int curr_time = time(nullptr);
+    int period_sec = 10;
+    _deposits.push_back(
+            Deposit{_get_free_id(), "1111 1111 1111 1111", 10, 5, curr_time, curr_time + period_sec, 100}
+    );
+}
+
+void DepositRepositoryInMemory::_add(const Deposit &deposit) const {
+    if(deposit._id == 0){
+        Deposit copy(deposit);
+        copy._id = _get_free_id();
+        _deposits.push_back(copy);
+    } else {
+        _deposits.push_back(deposit);
+    }
+}
+
+void DepositRepositoryInMemory::_remove(const Deposit &deposit) const {
+    _deposits = _get_list(Specification<Deposit>([&] (const Deposit& d) { return d._id != deposit._id;}));
+}
+
+vector<Deposit> DepositRepositoryInMemory::_get_list(const Specification<Deposit> &deposit_specification) const {
+    vector<Deposit> result;
+    for (const Deposit& deposit : _deposits) {
+        if (deposit_specification.filter(deposit)) {
+            result.push_back(deposit);
+        }
+    }
+    return result;
+}
+
+int DepositRepositoryInMemory::_get_free_id() const {
+    return _freeId++;
+}
--- backend/deposit/deposit_repository_in_memory.h
+++ backend/deposit/deposit_repository_in_memory.h
@@ -12,31 +12,17 @@ class DepositRepositoryInMemory : public DepositRepositoryInterface, public Sing
 
 private:
     mutable vector<Deposit> _deposits;
+    static int _freeId;
 
-    DepositRepositoryInMemory() {
-        int curr_time = time(nullptr);
-        _deposits.push_back(
-                Deposit{1, "1111 1111 1111 1111", 10, 5, curr_time, curr_time+5, 100}
-        );
-    }
+    DepositRepositoryInMemory();
 
-    void _add(const Deposit& deposit) const override {
-        _deposits.push_back(deposit);
-    }
+    void _add(const Deposit& deposit) const override;
 
-    void _remove(const Deposit& deposit) const override {
-        _deposits = _get_list(Specification<Deposit>([&] (const Deposit& d) { return d._id != deposit._id;}));
-    }
+    void _remove(const Deposit& deposit) const override;
 
-    vector<Deposit> _get_list(const Specification<Deposit>& deposit_specification) const override {
-        vector<Deposit> result;
-        for (const Deposit& deposit : _deposits) {
-            if (deposit_specification.filter(deposit)) {
-                result.push_back(deposit);
-            }
-        }
-        return result;
-    };
+    vector<Deposit> _get_list(const Specification<Deposit>& deposit_specification) const override;
+
+    int _get_free_id() const;
 };
 
 #endif //QUADROBANK_DEPOSIT_REPOSITORY_IN_MEMORY_H
