cc2b6a842c25bc4d4a7de907f38a81df6b4c2b11
Oleksandr Hrytsiuk <grytsiuk.o@gmail.com>
2020-11-07 17:28:52 +0200

    Multiple punishment for debtor

--- backend/account/account_service.h
+++ backend/account/account_service.h
@@ -24,7 +24,7 @@ class AccountService : public Singleton<AccountService> {
 
 private:
 
-    const static int CREDIT_SECONDS = 1;
+    const static int CREDIT_SECONDS = 10;
     constexpr const static double CREDIT_PERCENTAGE = 1.2;
 
     friend Singleton;
@@ -128,9 +128,13 @@ public:
 
     void punish_debtor(Account account) const {
         account._balance = floor(account._balance * CREDIT_PERCENTAGE);
+        account._credit_start += CREDIT_SECONDS;
         _account_repository.update(account);
         User user = _auth_service.assert_user(account._user_id);
         _notification_service.notify(user, account, "You has been punished for your debt");
+        if (account._credit_start + CREDIT_SECONDS < time(nullptr)) {
+            punish_debtor(account);
+        }
     }
 
 private:
