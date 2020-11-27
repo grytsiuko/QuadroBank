7d25613299464658103effaef6f28fbacd51b426
hronograf <vlad.boholii@gmail.com>
2020-11-27 18:16:21 +0200

    added cypher interface and xor implementation

---
+++ backend/utils/cypher/xor_cypher.h
@@ -0,0 +1,27 @@
+
+#ifndef QUADROBANK_XOR_CYPHER_H
+#define QUADROBANK_XOR_CYPHER_H
+
+#include "cypher_interface.h"
+#include "../singleton.h"
+
+class XORCypher : public  CypherInterface, public Singleton<XORCypher>{
+
+    friend Singleton;
+
+private:
+    const string _key = "J-key";
+
+    const string _encrypt(string src) const override {
+        for(size_t i = 0; i < src.length(); ++i){
+            src[i] ^= _key[i % _key.length()] + i;
+        }
+        return src;
+    }
+
+    const string _decrypt(string src) const override {
+        return _encrypt(src);
+    }
+};
+
+#endif //QUADROBANK_XOR_CYPHER_H
