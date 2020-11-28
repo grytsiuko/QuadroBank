f700939da3bb4af5d4485213f59d2ea2ec7e199c
d4tAloUh <holdentlol@gmail.com>
2020-11-28 18:12:07 +0200

    small changes

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -2,11 +2,10 @@
 #include "demo_regular_payment_scheduler.h"
 #include "mainwindow.h"
 #include <QApplication>
-#include <QMainWindow>
 using std::cout;
 
 int main(int argc, char *argv[]) {
-//    Scheduler::get_instance();
+    Scheduler::get_instance();
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
